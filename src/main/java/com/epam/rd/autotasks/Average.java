09b83a50ccc82a142e9fd8eec8cbbfd377dd1360
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-03-12 22:09:16 +0200

    done

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -1,12 +1,24 @@
 package com.epam.rd.autotasks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Average {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int sum = 0;
+        int count = 0;
 
+        while (value != 0) {
+            sum += value;
+            count++;
+            value = scanner.nextInt();
+        }
+
+        int averageValue = sum / count;
+        System.out.println(averageValue);
     }
-
 }
